2d2798097bc22af3bbfbd228ff3b88e0e7701ff7
SaulBG99 <a01282698@itesm.mx>
2018-08-25 18:21:10 -0500

    COnfiguration page layout

--- app/src/main/java/com/appemergencias/ConfigurationActivity.java
+++ app/src/main/java/com/appemergencias/ConfigurationActivity.java
@@ -1,13 +1,34 @@
 package com.appemergencias;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ConfigurationActivity extends AppCompatActivity {
+
+    ImageButton back;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(this);
+
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.back:
+                startActivity(new Intent(context, MainActivity.class));
+                break;
+        }
     }
 }
